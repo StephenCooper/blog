8b5968b2cc93a64991cbe68e1552c9c127d523f7
Stephen Cooper <scooperdev@gmail.com>
2023-02-08 23:15:37 +0000

    start reusing code

---
+++ src/data/blog-posts/DevApiService.ts
@@ -0,0 +1,27 @@
+export async function loadArticle(articleId: string) {
+    return await fetch("https://dev.to/api/articles/" + articleId, {
+        method: "GET",
+        headers: {
+            Accept: "application/vnd.forem.api-v1+json",
+        },
+    })
+        .then((response) => response.json())
+        .catch((err) => console.error(err));
+}
+
+export async function loadAllArticles(organization: string | undefined = undefined) {
+    return await fetch("https://dev.to/api/articles?username=scooperdev", {
+        method: "GET",
+        headers: {
+            Accept: "application/vnd.forem.api-v1+json",
+        },
+    })
+        .then((response) => response.json())
+        .then(
+            (response) => response
+                .filter((e: any) => (organization == undefined && e.organization?.name !== 'AG Grid')
+                    || (e.organization?.name === organization))
+        )
+        .catch((err) => console.error(err));
+
+}
