a91a37b6fd6b6ea4231fd2e40fd5d11f159fb163
Stephen Cooper <scooperdev@gmail.com>
2023-06-28 23:21:18 +0100

    small fixes

--- src/data/blog-posts/DevApiService.ts
+++ src/data/blog-posts/DevApiService.ts
@@ -1,4 +1,5 @@
 export async function loadArticle(articleId: string) {
+
     return await fetch("https://dev.to/api/articles/" + articleId, {
         method: "GET",
         headers: {
@@ -8,6 +9,36 @@ export async function loadArticle(articleId: string) {
         .then((response) => response.json())
         .catch((err) => console.error(err));
 }
+
+function addMonths(date: Date, months: number) {
+    var d = date.getDate();
+    date.setMonth(date.getMonth() + +months);
+    if (date.getDate() != d) {
+        date.setDate(0);
+    }
+    return date;
+}
+
+/* export async function loadArticleStats(articleId: string) {
+    ///https://dev.to/api/analytics/historical?start=2023-05-28&article_id=834021
+    const lastMonth = addMonths(new Date(), -1).toISOString().split('T')[0];
+    return await fetch(`https://dev.to/api/analytics/historical?start=${lastMonth}&article_id=${articleId}`, {
+        method: "GET",
+        headers: {
+            Accept: "application/vnd.forem.api-v1+json",
+            'api-key': import.meta.env.DEV_API_KEY
+        },
+    })
+        .then((response) => response.json())
+        .then((byDate: any) => {
+            const dateViews = Object.entries(byDate).map(([key, value]: any) => {
+                return value?.page_views?.total;
+            });
+            const views = dateViews.reduce((a: number, b: number) => a + b, 0);
+            return views;
+        })
+        .catch((err) => console.error(err));
+} */
 
 export async function loadAllArticles(organization: string | undefined = undefined) {
     return await fetch("https://dev.to/api/articles?username=scooperdev", {
